b3192b53c8faeaa484472798222abccf8e393974
felix Cotrina <felixcotrinavp@gmail.com>
2025-04-09 10:19:02 -0500

    segundo commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenido al curso de c#");
+            Console.WriteLine("mi segundo intento");
             Console.ReadKey();
         }
     }
